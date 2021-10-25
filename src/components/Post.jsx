e155f07260e9f4d2c69f40075cd4a389ab6c634b
Breno <bhfs.2908@gmail.com>
2021-10-25 18:37:42 -0300

    feat:finish

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,4 +9,43 @@ import {
   Typography,
 } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  card: {
+    marginBottom: theme.spacing(5),
+  },
+  media: {
+    height: 250,
+    [theme.breakpoints.down("sm")]: {
+      height: 150,
+    },
+  },
+}));
+
+const Post = ({ img, title }) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={img} title="My Post" />
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            {title}
+          </Typography>
+          <Typography variant="body2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam consectetur earum est.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Post;
